5e36e139ff241a55bac54cfd2a9ab89939420988
pooria <pouriyatajmehrabi1381@gmail.com>
2021-09-20 23:55:40 +0430

    done

--- digiapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class RegisterationForm(forms.Form):
-    username=forms.CharField(max_length=200)
-    password=forms.CharField(max_length=200)
-    confirm_password=forms.CharField(max_length=200)
-class LoginForm(forms.Form):
-    username=forms.CharField(max_length=200)
-    password=forms.CharField(max_length=200)
